963dd976b0882746e328aa655956263ead18027b
rupesh <rpiwal02@gmail.com>
2026-03-17 19:42:43 +0530

    updated resume

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,7 +10,7 @@ export const siteConfig: SiteConfig = {
   available: true,
   availableText: "Available for Work",
   resumeUrl:
-    "https://drive.google.com/file/d/1vIx7eJhZX61NfjTNTwfk3i3umQnEEx8S/view?usp=sharing",
+    "https://drive.google.com/file/d/1fsQo1VG6W_Kh0kTXO53FjMPdqXqkMsTx/view?usp=sharing",
   socials: [
     {
       name: "GitHub",
